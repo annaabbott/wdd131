5c8f8954caf15e931b5a6c7a8c0432c12f5e9194
Anna Abbott <annaabbottdesign@gmail.com>
2025-01-21 20:05:00 -0600

    mobile view working

---
+++ scripts/place.js
@@ -0,0 +1,25 @@
+const currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+
+const modified = new Date(document.lastModified);
+
+const temp = 35;
+const windSpeed = 8;
+
+document.getElementById("currentyear").innerHTML = currentYear;
+document.getElementById("lastModified").innerHTML = modified;
+
+function calcWindChill(temp, windSpeed) {
+  windChill =
+    35.74 +
+    0.6215 * temp -
+    35.75 * windSpeed ** 0.16 +
+    0.4275 * temp * windSpeed ** 0.16;
+  roundedWindChill = parseFloat(windChill.toFixed(1));
+  return roundedWindChill;
+}
+
+document.querySelector("#windChill").textContent = `${calcWindChill(
+  temp,
+  windSpeed
+)} °F`;

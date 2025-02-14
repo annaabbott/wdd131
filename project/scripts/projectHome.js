e959067611dd7368638257725c6939c906e769f7
Anna Abbott <annaabbottdesign@gmail.com>
2025-02-14 10:46:55 -0600

    added local storage to form/thanks pages

--- project/scripts/projectHome.js
+++ project/scripts/projectHome.js
@@ -5,4 +5,3 @@ const modified = new Date(document.lastModified);
 
 document.getElementById("currentyear").innerHTML = currentYear;
 document.getElementById("lastModified").innerHTML = modified;
-
--- project/scripts/projectThanks.js
+++ project/scripts/projectThanks.js
@@ -6,9 +6,9 @@ const modified = new Date(document.lastModified);
 document.getElementById("currentyear").innerHTML = currentYear;
 document.getElementById("lastModified").innerHTML = modified;
 
-console.log("javascript loaded")
-
+const visitCount = document.querySelector(".myVisitCount");
 let numVisits = Number(window.localStorage.getItem("myVisitCount")) || 0;
 console.log(numVisits);
-numVisits++;
-localStorage.setItem("myVisitCount", numVisits);
+if (numVisits >= 1) {
+  visitCount.textContent = `Glad to see you again!`;
+}
